c6058deeebaaafdbe5bfef24ec595434157e3c0b
eccyan <g00.eccyan@gmail.com>
2020-07-08 16:17:38 +0900

    Add deepl alias

--- fish/.config/fish/aliases.fish
+++ fish/.config/fish/aliases.fish
@@ -1,2 +1,5 @@
-alias vi 'nvim'
-alias ack 'pt'
+alias vi='nvim'
+alias ack='pt'
+alias pbcopy='xsel --clipboard --input'
+alias pbpaste='xsel --clipboard --output'
+alias deepl="tr \"\n\" \" \" | xargs echo 'http://www.deepl.com/translator#en/ja/' | xargs -I {} xdg-open '{}'"
