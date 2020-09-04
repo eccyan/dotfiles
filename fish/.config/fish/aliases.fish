98926abab7902b7e8f332fb74f26263889dc0b6f
eccyan <g00.eccyan@gmail.com>
2020-09-04 14:42:51 +0900

    Alias pbcopy,pbpaste only linux

--- fish/.config/fish/aliases.fish
+++ fish/.config/fish/aliases.fish
@@ -1,5 +1,10 @@
 alias vi='nvim'
 alias ack='pt'
-alias pbcopy='xsel --clipboard --input'
-alias pbpaste='xsel --clipboard --output'
 alias deepl="tr \"\n\" \" \" | xargs echo 'http://www.deepl.com/translator#en/ja/' | xargs -I {} xdg-open '{}'"
+
+switch (uname)
+  case Linux
+    alias pbcopy='xsel --clipboard --input'
+    alias pbpaste='xsel --clipboard --output'
+  case '*'
+end
